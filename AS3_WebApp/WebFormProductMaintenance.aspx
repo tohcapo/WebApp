1e982cad2e047eb48dc152a1628a690383661276
Luke4397 <Luke4397@gmail.com>
2015-10-27 10:51:30 +1000

    Product Maintenance form

--- AS3_WebApp/WebFormProductMaintenance.aspx
+++ AS3_WebApp/WebFormProductMaintenance.aspx
@@ -4,11 +4,32 @@
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
-    <title></title>
+    <title>Product Maintenance</title>
 </head>
 <body>
     <form id="form1" runat="server">
     <div>
+        <asp:HyperLink ID="HyperLink1" runat="server" NavigateUrl="~/WebFormMain.aspx">Back</asp:HyperLink>
+        <table border="1">
+            <tr style="font-weight:bold">
+                <td style="width:200px">Product Code</td>
+                <td style="width:200px">Name</td>
+                <td style="width:200px">Version</td>
+                <td style="width:200px">Release Date</td>
+            </tr>
+            <tr>
+                <td>DRAFT10</td>
+                <td>Draft Manager 1.0</td>
+                <td>1.0</td>
+                <td>12/1/2010</td>
+                <td>
+                    <asp:Button ID="buttonEdit" runat="server" Text="Edit" />
+                </td>
+                <td>
+                    <asp:Button ID="buttonDelete" runat="server" Text="Delete" />
+                </td>
+            </tr>
+        </table>
     </div>
     </form>
 </body>
--- AS3_WebApp/WebFormProductMaintenance.aspx.designer.cs
+++ AS3_WebApp/WebFormProductMaintenance.aspx.designer.cs
@@ -20,5 +20,32 @@ namespace AS3_WebApp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// HyperLink1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLink1;
+        
+        /// <summary>
+        /// buttonEdit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttonEdit;
+        
+        /// <summary>
+        /// buttonDelete control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button buttonDelete;
     }
 }
