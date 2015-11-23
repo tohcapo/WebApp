f28d86335240356c066c0e8462855f0c92f80586
memessss <zcmenroe@gmail.com>
2015-11-23 10:17:49 +1000

    Technician Maintenance

--- AS3_WebApp/WebFormTechnicianMaintenance.aspx.cs
+++ AS3_WebApp/WebFormTechnicianMaintenance.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
 
 namespace AS3_WebApp
 {
@@ -12,6 +13,41 @@ namespace AS3_WebApp
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void BindSqlDataSources()
+        {
+            SqlDataSource1.SelectCommand = "SELECT [TechID], [Name], [Email], [Phone] FROM [Technicians]";
+
+            SqlDataSource1.DataBind();
+            GridViewTechnicians.DataBind();
+
+            Response.Redirect(Request.RawUrl);
+        }
+
+        private void InsertTechnician()
+        {
+            using (SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["TechSupportConnectionString"].ConnectionString))
+            {
+                connection.Open();
+                using (SqlCommand cmd = new SqlCommand(@"INSERT INTO [Technicians] (Name, Email, Phone) VALUES (@Name, @Email, @Phone);", connection))
+                {
+                    cmd.Parameters.AddWithValue("@Name", txtName.Text);
+                    cmd.Parameters.AddWithValue("@Email", txtEmail.Text);
+                    cmd.Parameters.AddWithValue("@Phone", txtPhone.Text);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        protected void btnAddTechnician_Click1(object sender, EventArgs e)
+        {
+            Response.Write("<script>alert('Technician Added.');</script>");
+
+            InsertTechnician();
+
+            BindSqlDataSources();
         }
     }
 }
--- AS3_WebApp/WebFormTechnicianMaintenance.aspx.designer.cs
+++ AS3_WebApp/WebFormTechnicianMaintenance.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace AS3_WebApp {
     public partial class WebFormTechnicianMaintenance {
         
         /// <summary>
-        /// GridView1 control.
+        /// GridViewTechnicians control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GridView1;
+        protected global::System.Web.UI.WebControls.GridView GridViewTechnicians;
         
         /// <summary>
         /// SqlDataSource1 control.
@@ -29,5 +29,41 @@ namespace AS3_WebApp {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSource1;
+        
+        /// <summary>
+        /// txtName control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtName;
+        
+        /// <summary>
+        /// txtEmail control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtEmail;
+        
+        /// <summary>
+        /// txtPhone control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txtPhone;
+        
+        /// <summary>
+        /// btnAddTechnician control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAddTechnician;
     }
 }
